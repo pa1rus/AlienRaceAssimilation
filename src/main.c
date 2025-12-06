35460f319fa1618b264b502bf07262736057f412
pa1rus <lewaldkrzysztof@gmail.com>
2025-12-06 21:12:25 +0100

    test

--- src/main.c
+++ src/main.c
@@ -2,11 +2,14 @@
 #include "globals.h"
 #include "game.h"
 #include "audio.h"
+#include "hermes.h"
 
 int main()
 {
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     InitWindow(GAME_WIDTH, GAME_HEIGHT, "Platformer");
+
+    hermesInit();
 
     InitGame();
 
